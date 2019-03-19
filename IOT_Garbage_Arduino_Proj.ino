2bd12ce4c84dc80264d7e165a4a47ccf04644c6e
Sonaal P. Pradeep <kingoftheland28@gmail.com>
2019-03-20 01:22:01 +0530

    Further modifications

--- IOT_Garbage_Arduino_Proj.ino
+++ IOT_Garbage_Arduino_Proj.ino
@@ -1,7 +1,19 @@
+// -------------------------------------------------------------------//
+//                | IOT ARDUINO GARBAGE LEVEL DETECTOR |              //
+// -------------------------------------------------------------------//
+
+/* by Sonaal P. Pradeep
+   IOT Arduino Garbage Level Detector project for our Enivormental Studies Course.
+   The aim of this project is to implement a solution which can be implemented by computer science students.
+*/
+
+
 #define pTrig 13
 #define pEcho 12
 #define lRed 11
 #define lGreen 10
+
+long capacity = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -12,37 +24,80 @@ void setup() {
 }
 
 void loop() {
-  long duration, distance;
-  
-  digitalWrite(pTrig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pTrig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pTrig, LOW);
-  
-  duration = pulseIn(pEcho, HIGH);
-  distance = (duration/2) / 29.1;
-  
-  if (distance < 15)
+  char ch;
+  long duration, distance, percent;
+
+  Serial.println("Garbage Level Detector");
+  delay(200);
+  Serial.println("Select from one amongst the following : ");
+  delay(200);
+  Serial.println("0. Configure the board for a new dustbin");
+  delay(200);
+  Serial.println("1. Display fill level");
+  delay(200);
+  Serial.println("--------------------------------------------");
+  delay(200);
+
+  front:
+  ch = Serial.read();
+
+  if (ch == '0')
   {
-    digitalWrite(lRed,HIGH);
-    digitalWrite(lGreen,LOW);
+      Serial.println("Reconfiguring dustbin");
+      delay(200);
+      if(capacity == 0)
+      {
+        Serial.println("New dustbin");
+        delay(200);
+      }
+        
+      else
+      {
+        Serial.print("Old size of dustbin : ");
+        Serial.print(capacity);
+        Serial.println(" cms");
+      }
+
+      capacity = triggerPulse();
+
+      Serial.print("New size of dustbin : ");
+      delay(200);
+      Serial.print(capacity);
+      delay(200);
+      Serial.println(" cms");      
+  }
+  else if (ch == '1')
+  {
+      Serial.println("Fill level");
+
+      distance = triggerPulse();
+      
+      delay(200);
+      Serial.print("Current level : ");
+      delay(200);
+      Serial.print(distance);
+      delay(200);
+      Serial.println(" cm");
   }
   else
-  {
-    digitalWrite(lRed,LOW);
-    digitalWrite(lGreen,HIGH);
-  }
-  
-  if (distance >= 200 || distance <= 0)
-  {
-    Serial.println("Out of range");
-  }
-  else
-  {
-    Serial.print(distance);
-    Serial.println(" cm");
-  }
-  
+      goto front;
+
+  ch = " ";
+
   delay(250);
+}
+
+long triggerPulse()
+{
+      long res, duration, distance;
+      digitalWrite(pTrig, LOW);
+      delayMicroseconds(2);
+      digitalWrite(pTrig, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(pTrig, LOW);
+
+      duration = pulseIn(pEcho, HIGH);
+      distance = (duration / 2) / 29.1;
+
+      return distance;
 }
